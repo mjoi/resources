711e0358457e8fd99b1f327207d962337be3d47c
nelsonkhan <nelsonkhan@gmail.com>
2016-04-26 19:15:51 -0600

    Update scriptList.js

--- scriptList.js
+++ scriptList.js
@@ -1,3 +1,9 @@
 {"scripts":[
-    {"url":"https://raw.githubusercontent.com/nelsonkhan/resources/master/iPlay.js"}
+    {"url":"https://raw.githubusercontent.com/nelsonkhan/resources/master/iPlay.js"},
+    {"url":"https://raw.githubusercontent.com/nelsonkhan/resources/master/loadRecord.js"}
+    {"url":"https://raw.githubusercontent.com/nelsonkhan/resources/master/monthsBetween.js"}
+    {"url":"https://raw.githubusercontent.com/nelsonkhan/resources/master/postImages.js"}
+    {"url":"https://raw.githubusercontent.com/nelsonkhan/resources/master/randWait.js"}
+    {"url":"https://raw.githubusercontent.com/nelsonkhan/resources/master/setVariables.js"}
+    {"url":"https://raw.githubusercontent.com/nelsonkhan/resources/master/update.js"}
 ]}
