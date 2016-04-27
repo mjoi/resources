038bfb8d0b0409b3a57afccab8ba4591481ad7e1
nelsonkhan <nelsonkhan@gmail.com>
2016-04-26 19:32:45 -0600

    Update randWait.js

--- randWait.js
+++ randWait.js
@@ -1,5 +1,5 @@
 function randWait() {
   // wait random # of seconds between 2 and 6
-  iimSet("seconds", Math.floor((Math.random() * 4) + 2));
-  iimPlay("CODE:WAIT SECONDS=" + seconds);
+  iimSet("seconds", Math.floor((Math.random() * 6) + 2));
+  iimPlay("CODE:WAIT SECONDS={{seconds}}");
 }
